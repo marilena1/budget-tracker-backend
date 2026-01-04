10891f50b711bb62e3d06f390f7bb7b19b97da0e
marilena <mariaeleni.sotirchou@gmail.com>
2026-01-04 23:48:19 +0100

    build: initialize Spring Boot project with core dependencies

---
+++ src/main/java/com/budgettracker/budget_tracker_backend/BudgetTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.budgettracker.budget_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerBackendApplication.class, args);
+	}
+
+}
