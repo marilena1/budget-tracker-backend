8e3f637b3ff4c0ae6d7bb5e88eb3b24e31104160
marilena <mariaeleni.sotirchou@gmail.com>
2026-01-08 15:05:55 +0100

    feat: Add validation to domain models
    - Add field validation to User, Category, and Transaction models

--- src/main/java/com/budgettracker/budget_tracker_backend/model/Category.java
+++ src/main/java/com/budgettracker/budget_tracker_backend/model/Category.java
@@ -1,5 +1,7 @@
 package com.budgettracker.budget_tracker_backend.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -14,9 +16,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "categories")
 public class Category extends AbstractEntity {
 
+    @NotBlank(message = "Category name is required")
+    @Size(min = 2, max = 50, message = "Name must be 2-50 characters")
     @Indexed(unique = true)
     private String name;
 
+    @Size(max = 200, message = "Description cannot exceed 200 characters")
     private String description;
+
     private String color;
 }
