1fc113c7a3923cc86af51937157fac16c43d032d
marilena <mariaeleni.sotirchou@gmail.com>
2026-01-26 19:23:25 +0100

    chore: remove unused MongoDB Query import from TransactionRepository

--- src/main/java/com/budgettracker/budget_tracker_backend/repository/TransactionRepository.java
+++ src/main/java/com/budgettracker/budget_tracker_backend/repository/TransactionRepository.java
@@ -4,7 +4,6 @@ import com.budgettracker.budget_tracker_backend.model.Transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
